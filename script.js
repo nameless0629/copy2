81cdec6ceb3827c6d38e4c1b5cfb878ce28218b9
D1149338 <D1149338@o365.fcu.edu.tw>
2024-06-04 09:09:41 +0800

    1

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var cookieInfoElement = document.querySelector('#cookie-info');
 
     cookieInfoElement.addEventListener('click', function() {
-        alert("您的瀏覽器必須啟動 Cookies 功能\n\n本站使用了兩個cookie： 必要的是連線cookie，通常叫做 MoodleSession。您必須在瀏覽器裡允許這個cookie，以便使您的登入在頁與頁之間連續有效。當您登離或關閉瀏覽器，這個cookie在瀏覽器和伺服器裡都會被刪除。 另一個cookie純粹是為了方便，通常被叫做類似 MOODLEID 之類的名字。它只用來在瀏覽器裡記住您的用戶名。這意味著當您再次回到本站，登入頁面裡的用戶名已經替您填好了。拒絕此cookie是安全的，只不過每次登入都要重新輸入用戶名而已。");
+        Swal.fire({
+            title: '<strong style="font-size: 1.5em;">您的瀏覽器必須啟動 Cookies 功能</strong>',
+            icon: 'info',
+            html: '本站使用了兩個cookie： 必要的是連線cookie，通常叫做 MoodleSession。您必須在瀏覽器裡允許這個cookie，以便使您的登入在頁與頁之間連續有效。當您登離或關閉瀏覽器，這個cookie在瀏覽器和伺服器裡都會被刪除。 另一個cookie純粹是為了方便，通常被叫做類似 MOODLEID 之類的名字。它只用來在瀏覽器裡記住您的用戶名。這意味著當您再次回到本站，登入頁面裡的用戶名已經替您填好了。拒絕此cookie是安全的，只不過每次登入都要重新輸入用戶名而已。',
+            showCloseButton: true,
+            focusConfirm: false,
+            confirmButtonText: 'OK',
+        });
     });
 });
